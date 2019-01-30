444d4c633df2a28fd3e92d6af2b8d2006d8fa010
Сергей Максутов <sergey.maxutov@ps.kz>
2019-01-30 14:38:08 +0600

    Added JWT.isJWT method

--- src/services/JWT.ts
+++ src/services/JWT.ts
@@ -8,15 +8,26 @@ export interface IJWTOptions {
 
 export default class JWT {
 
+    private static jwtTokenRegExp: RegExp = /^([A-Za-z0-9-_=]+)\.([A-Za-z0-9-_=]+)(?:\.([A-Za-z0-9-_.+/=]*))?$/;
+
     public static DEFAULT_ALGO = "RS256";
 
     public static secret = Env.APP_SECRET;
 
     public static sign(payload: any, algorithm = this.DEFAULT_ALGO, secret = this.secret): string {
-        return jwt.sign(payload, secret, {algorithm});
+        return jwt.sign(payload, secret, { algorithm });
     }
 
     public static verify<T = any>(token: string, algorithm = this.DEFAULT_ALGO, secret = this.secret): T {
-        return jwt.verify(token, secret, {algorithms: [algorithm]}) as unknown as T;
+        return jwt.verify(token, secret, { algorithms: [algorithm] }) as unknown as T;
+    }
+
+    /**
+     * Проверяем, является ли токен JWT
+     *
+     * @param {*} token
+     */
+    public static isJWT(token: string): boolean {
+        return this.jwtTokenRegExp.test(token);
     }
 }
